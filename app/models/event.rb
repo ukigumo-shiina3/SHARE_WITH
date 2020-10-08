d04834920fa2acfc1d6190aa3dcc48843fa1b107
Daisuke Tanaka <daisukeyao@outlook.jp>
2020-10-08 21:00:39 +0900

    [Add]association,validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,10 @@
 class Event < ApplicationRecord
+    has_many :favorites, dependent: :destroy
+    has_many :comments, dependent: :destroy
+    belongs_to :user
+    belongs_to :genre,optional: true
+    attachment :image
+
+    validates :title, presence: true
+	validates :body, presence: true
 end
