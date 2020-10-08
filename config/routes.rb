46e0ab77730d6221e745b041ec8afcb5bb45b46d
Daisuke Tanaka <daisukeyao@outlook.jp>
2020-10-08 17:59:04 +0900

    [Add]routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'messages/create'
-  get 'messages/destroy'
-  get 'rooms/create'
-  get 'rooms/show'
-  get 'genres/index'
-  get 'favorites/destroy'
-  get 'favorites/create'
-  get 'comments/create'
-  get 'comments/destroy'
-  get 'events/index'
-  get 'events/create'
-  get 'events/edit'
-  get 'events/update'
-  get 'events/destroy'
-  get 'homes/index'
-  get 'homes/about'
-  get 'homes/howto'
-  get 'users/index'
-  get 'users/edit'
-  get 'users/show'
-  get 'users/update'
-  get 'users/destroy'
+  root 'home#top'  
+  get 'home/about'
+  get 'home/howto'
+
+  resources :users, only: [:index, :edit, :show, :update,:destroy] 
+
+  resources :events, except: [:new] do
+    resource :favorites, only: [:create, :destroy]
+    resources :comments, only: [:create, :destroy]
+  end
+  resources :genres, only: [:index]
+  resources :rooms, only: [:create, :show]
+  resources :messages, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
