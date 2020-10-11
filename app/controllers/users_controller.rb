aab7ee87cc806d0d3b9a797d9aa8fcc3fa21cd6f
Daisuke Tanaka <daisukeyao@outlook.jp>
2020-10-11 15:45:44 +0900

    [Add]アクション

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,40 @@
 class UsersController < ApplicationController
   def index
-  end
-
-  def edit
+    @user= current_user
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+    if @user.id != current_user.id
+        redirect_to user_path(current_user.id)
+    end
+  end
+  
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+        flash[:notice] = "更新に成功しました。"
+        redirect_to user_path
+    else
+        render:edit
+    end
   end
 
   def destroy
+    @user = User.find(params[:id])
+    if @user.id != current_user.id
+        redirect_to user_path(current_user.id)
+        flash[:notice] = "削除に成功しました。"
+    end
+  end
+
+  private
+  def user_params
+      params.require(:user).permit(:name, :introduction, :avator_image)  
   end
 end
