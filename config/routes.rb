1c561ce38ee5a5fe846a1a016f325e18a4b0725a
Daisuke Tanaka <daisukeyao@outlook.jp>
2020-10-14 14:21:18 +0900

    [Fix]routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'search/search'
   devise_for :users
   root 'events#index'  
   get 'home/about', to: 'homes#about'
   get 'home/howto', to: 'homes#howto'
   post '/events/guest_sign_in', to: 'events#new_guest'
+  get '/search' => 'search#search'
 
   resources :users, only: [:index, :edit, :show, :update,:destroy] 
   resources :events do
@@ -11,7 +13,8 @@ Rails.application.routes.draw do
     resources :comments, only: [:create, :destroy]
   end
   resources :genres, only: [:index]
-  resources :rooms, only: [:create, :show]
+  resources :rooms, only: [:create, :show, :index]
   resources :messages, only: [:create, :destroy]
+  resources :schedule, only: [:create, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
