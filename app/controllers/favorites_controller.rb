f7b111715bfc8973eb41b648045dc9eccf9855c1
Daisuke Tanaka <daisukeyao@outlook.jp>
2020-10-20 23:36:27 +0900

    [Add]favorite_ajax

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,13 @@
 class FavoritesController < ApplicationController
   def create
-    @event = Event.find(params[:event_id])
+    @event = Event.find(params[:event_id]) 
     favorite = current_user.favorites.new(event_id: @event.id)
     favorite.save
-    redirect_to events_path
   end
 
   def destroy
     @event = Event.find(params[:event_id])
-    favorite = current_user.favorites.new(event_id: @event.id)
+    favorite = current_user.favorites.find_by(event_id: @event.id)
     favorite.destroy
-    redirect_to events_path
   end
 end
