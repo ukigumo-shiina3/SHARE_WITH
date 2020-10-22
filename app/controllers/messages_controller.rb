0e2d8334535a72acb13d4a9414766ef261142fd5
Daisuke Tanaka <daisukeyao@outlook.jp>
2020-10-22 20:02:51 +0900

    [Fix]DM_function

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,12 +2,17 @@ class MessagesController < ApplicationController
   def create
     if Entry.where(:user_id => current_user.id, :room_id => params[:message][:room_id]).present?
       @message = Message.create(params.require(:message).permit(:user_id, :content, :room_id).merge(:user_id => current_user.id))
-      redirect_to "/rooms/#{@message.room_id}"
     else
-      redirect_back(fallback_location: root_path)
+      flash[:alert] = "メッセージ送信に失敗しました。"
     end
+      redirect_to "/rooms/#{@message.room_id}"
   end
 
   def destroy
+      @message = Message.find(params[:id])
+      room_id = @message.room_id
+      @message.destroy
+      flash[:alert] = "メッセージを削除しました。"
+      redirect_to "/rooms/#{room_id}"
   end
 end
