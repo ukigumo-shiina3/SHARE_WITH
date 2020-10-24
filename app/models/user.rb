8276ecbf53829d7552ace420dc30bce2d097338f
Daisuke Tanaka <daisukeyao@outlook.jp>
2020-10-24 23:33:24 +0900

    [Fix]event_rank

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,16 +13,9 @@ class User < ApplicationRecord
 
   attachment :avator_image
 
-  # def self.guest
-  #   find_or_create_by!(email: 'guest@example.com') do |user|
-  #     user.password = SecureRandom.urlsafe_base64
-  #   end
-  # end
-
-  validates :name, :email, :introduction, :avator_image_id, presence: true
+  validates :name, :email, :introduction, :avator_image, presence: true
 
   def self.create_all_ranks 
-    # Event.find(Favorite.group(:event_id).order('count(event_id) desc').limit(3).pluck(:event_id))
     Event.find(Favorite.group(:event_id).order(Arel.sql("LOWER(event_id) desc")).limit(3).pluck(:event_id))
   end
 end
