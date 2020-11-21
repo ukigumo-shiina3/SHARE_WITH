29bdbf91995780391bf1f5bb669bbaecfc55c134
Daisuke Tanaka <daisukeyao@outlook.jp>
2020-11-21 10:52:14 +0900

    [Add]model_test

--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Entry, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "アソシエーションのテスト" do
+    context "Userモデルとの関係" do
+      it "N:1となっている" do
+        expect(Entry.reflect_on_association(:user).macro).to eq :belongs_to
+      end
+    end
+    context "Roomモデルとの関係" do
+      it "N:1となっている" do
+        expect(Entry.reflect_on_association(:room).macro).to eq :belongs_to
+      end
+    end
+  end
 end
