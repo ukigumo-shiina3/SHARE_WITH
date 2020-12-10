270536805cb6ccb1ca9e12472f1bd4815e0307a4
Daisuke Tanaka <daisukeyao@outlook.jp>
2020-12-10 20:31:46 +0900

    [Add]explanation

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,18 +1,17 @@
 class EventsController < ApplicationController
-
   before_action :authenticate_user!, except: [:new_guest]
-  
-  def new 
+
+  def new
     @event = Event.new
   end
-  
+
   def create
     @event = Event.new(event_params)
-    @event.user_id = current_user.id 
-    post_date = params["event"]["event_date(1i)"] << "-" << params["event"]["event_date(2i)"] << "-" << params["event"]["event_date(3i)"]
+    @event.user_id = current_user.id
+    post_date = params['event']['event_date(1i)'] << '-' << params['event']['event_date(2i)'] << '-' << params['event']['event_date(3i)'] #post_date = イベント新規登録時の日程フォームの年(1i),月(2i),日(3i)
     @event.event_date = post_date.to_date
     if @event.save
-      flash[:notice] = "イベントを作成しました。"
+      flash[:notice] = 'イベントを作成しました。'
       redirect_to events_path
     else
       render 'new'
@@ -26,39 +25,37 @@ class EventsController < ApplicationController
       @events = Event.all.includes(:favorites, :comments).page(params[:page]).per(7).reverse_order
       @all_ranks = Event.create_all_ranks
     else
-      @events = Array.new #ログインしてない状態のトップページ表示
-      @all_ranks = Array.new
+      @events = []   # ログインしてない状態のトップページ表示
+      @all_ranks = []
     end
   end
 
   def show
-    @event = Event.find(params[:id]) 
-    @comments = @event.comments  
-    @user = @event.user 
+    @event = Event.find(params[:id])
+    @comments = @event.comments
+    @user = @event.user
     @comment = Comment.new
   end
 
   def edit
     @event = Event.find(params[:id])
-    if @event.user_id != current_user.id
-        redirect_to events_path
-    end
+    redirect_to events_path if @event.user_id != current_user.id
   end
 
   def update
     @event = Event.find(params[:id])
     if @event.update(event_params)
-        flash[:notice] = "イベントを更新しました。"
-        redirect_to events_path
+      flash[:notice] = 'イベントを更新しました。'
+      redirect_to events_path
     else
-        render:edit
+      render :edit
     end
   end
 
   def destroy
     @event = Event.find(params[:id])
     @event.destroy
-    flash[:notice] = "イベントを削除しました。"
+    flash[:notice] = 'イベントを削除しました。'
     redirect_to events_path
   end
 
@@ -72,7 +69,8 @@ class EventsController < ApplicationController
   end
 
   private
-    def event_params
-      params.require(:event).permit(:user_id, :genre_id, :title, :body, :image, :open_hour, :open_minute,:end_hour,:end_minute, :recruitment)
-    end
+
+  def event_params
+    params.require(:event).permit(:user_id, :genre_id, :title, :body, :image, :open_hour, :open_minute, :end_hour, :end_minute, :recruitment)
+  end
 end
